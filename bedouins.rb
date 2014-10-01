122a4224371fd6d872e6e9b5e2af4fabd16efd18
Podlabs <damon@podlabs.com>
2014-09-30 21:06:16 -0500

    members stuff

--- bedouins.rb
+++ bedouins.rb
@@ -1,9 +1,19 @@
 #!/usr/bin/env ruby
 
-require './env.rb'
+require 'rubygems'
+require 'faster_csv'
+require 'activesupport'
 
-t = TwitterApi.new
+fn = "members.csv"
 
-account = "cafebdouins"
+ignores = ["thirdprestigeco","capitalfactory","railsvitamins"]
 
+hashes = []
+FasterCSV.foreach(fn, :headers => true, :col_sep => ',') do |row|
+  row_hash = row.to_hash
+  hashes << {"name" => row_hash["name"], "screen_name" => row_hash["screen_name"], "image_url" => row_hash["image"].gsub("normal","bigger")} unless ignores.include?(row_hash["screen_name"].downcase)
+end
 
+h = {"members" => hashes.shuffle[0..19]}
+
+File.open("members.json", 'w') { |file| file.write(h.to_json)}
