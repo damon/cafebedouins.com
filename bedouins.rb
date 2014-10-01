cddef550d04557b7719c0713447bffee6bf95733
Podlabs <damon@podlabs.com>
2014-09-30 21:09:11 -0500

    dynamic shit

--- bedouins.rb
+++ bedouins.rb
@@ -4,16 +4,20 @@ require 'rubygems'
 require 'faster_csv'
 require 'activesupport'
 
-fn = "members.csv"
+class Bedouins
+  def self.execute
+    fn = "members.csv"
 
-ignores = ["thirdprestigeco","capitalfactory","railsvitamins"]
+    ignores = ["thirdprestigeco","capitalfactory","railsvitamins"]
 
-hashes = []
-FasterCSV.foreach(fn, :headers => true, :col_sep => ',') do |row|
-  row_hash = row.to_hash
-  hashes << {"name" => row_hash["name"], "screen_name" => row_hash["screen_name"], "image_url" => row_hash["image"].gsub("normal","bigger")} unless ignores.include?(row_hash["screen_name"].downcase)
+    hashes = []
+    FasterCSV.foreach(fn, :headers => true, :col_sep => ',') do |row|
+      row_hash = row.to_hash
+      hashes << {"name" => row_hash["name"], "screen_name" => row_hash["screen_name"], "image_url" => row_hash["image"].gsub("normal","bigger")} unless ignores.include?(row_hash["screen_name"].downcase)
+    end
+
+    h = {"members" => hashes.shuffle[0..19]}
+
+    File.open("members.json", 'w') { |file| file.write(h.to_json)}
+  end
 end
-
-h = {"members" => hashes.shuffle[0..19]}
-
-File.open("members.json", 'w') { |file| file.write(h.to_json)}
--- hello.rb
+++ hello.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
+require 'bedouins'
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
+  Bedouins.execute
 end
