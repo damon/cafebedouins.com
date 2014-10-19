2841da272a254f960ad3c34f490a44879fef3bae
Podlabs <damon@podlabs.com>
2014-10-18 23:42:19 -0500

    simple

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source 'https://rubygems.org'
 
 ruby '2.0.0'
 
-gem 'sinatra'
+gem 'sinatra', require: 'sinatra/base'
 gem 'grackle','>=0.3.0'
 gem 'activesupport', '~> 3.2.x'
-gem 'sinatra-index'
+gem 'thin'
 
--- hello.rb
+++ hello.rb
@@ -1,12 +1,11 @@
 require 'sinatra'
-require 'sinatra-index'
 
 load File.join(File.dirname(__FILE__), 'env.rb')
 
 class Hello < Sinatra::Base
-  register Sinatra::Index
-  use_static_index 'index.html'
-
+  get '/' do
+    send_file File.join(settings.public_folder, 'index.html')
+  end
   get '/members.json' do  
     Bedouins.execute  
   end
