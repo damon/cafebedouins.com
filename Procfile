ef22d44dc5d0617bd78c54e027893b9eb3757012
Podlabs <damon@podlabs.com>
2014-10-19 00:19:52 -0500

    heroku tweakz

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec ruby hello.rb -p $PORT
+web: bundle exec rackup config.ru -p $PORT
--- hello.rb
+++ hello.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 load File.join(File.dirname(__FILE__), 'env.rb')
 
-class Hello < Sinatra::Application
+class Hello < Sinatra::Base
   get '/' do
     send_file File.join(settings.public_folder, 'index.html')
   end
