7ad7472515660ac2d92a696680398b87138c112c
Podlabs <damon@podlabs.com>
2014-10-18 23:24:41 -0500

    hey, sometimes it helps to read instructions

--- hello.rb
+++ hello.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
+require 'sinatra-index'
 
 load File.join(File.dirname(__FILE__), 'env.rb')
 
 class Hello < Sinatra::Base
+  register Sinatra::Index
+  use_static_index 'index.html'
+
   get '/members.json' do  
     Bedouins.execute  
   end
