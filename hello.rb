cb79919f1682d8a0f7498b16e8f8aa0f2cd2421f
Podlabs <damon@podlabs.com>
2014-10-18 23:18:02 -0500

    dynamic members

--- hello.rb
+++ hello.rb
@@ -3,8 +3,10 @@ require 'sinatra'
 load File.join(File.dirname(__FILE__), 'env.rb')
 
 class Hello < Sinatra::Base
-  enable :static
+  set :root, File.expand_path("#{File.dirname(__FILE__)}")
   
+  enable :static
+    
   # get '/' do
   #   send_file File.join(settings.public_folder, 'index.html')
   # end
