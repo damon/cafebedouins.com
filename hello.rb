bb0f9796bb7bb3c29c46d31c7e9eae8f692b633b
Podlabs <damon@podlabs.com>
2014-10-19 00:16:45 -0500

    heroku run weirdness

--- hello.rb
+++ hello.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 load File.join(File.dirname(__FILE__), 'env.rb')
 
-class Hello < Sinatra::Base
+class Hello < Sinatra::Application
   get '/' do
     send_file File.join(settings.public_folder, 'index.html')
   end
