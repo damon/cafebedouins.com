4088099a83fa65ba854d152b8d72f2b94cbbf651
Podlabs <damon@podlabs.com>
2014-10-18 22:58:20 -0500

    start to serve JSON file in Sinatra

--- hello.rb
+++ hello.rb
@@ -6,3 +6,7 @@ get '/' do
   send_file File.join(settings.public_folder, 'index.html')
   Bedouins.execute
 end
+
+get '/members.json' do
+  "{\"members\":[{\"name\":\"Mayer Seidman\",\"image_url\":\"http://pbs.twimg.com/profile_images/481833987430551552/oUzvpgPm_bigger.jpeg\",\"screen_name\":\"MayerSeidman\"}"
+end
