8e035d5540d8f0d379ade294b7835b79c02102b8
Podlabs <damon@podlabs.com>
2014-10-18 23:15:23 -0500

    dynamic members

--- hello.rb
+++ hello.rb
@@ -2,10 +2,14 @@ require 'sinatra'
 
 load File.join(File.dirname(__FILE__), 'env.rb')
 
-#get '/' do
-#  send_file File.join(settings.public_folder, 'index.html')
-#end
+class Hello < Sinatra::Base
+  enable :static
+  
+  # get '/' do
+  #   send_file File.join(settings.public_folder, 'index.html')
+  # end
 
-get '/members.json' do  
-  Bedouins.execute  
+  get '/members.json' do  
+    Bedouins.execute  
+  end
 end
